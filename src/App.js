3ee66b74bff1add0d9979db6b781fec16815d66e
Calvin Bagby <cbagby33@gmail.com>
2026-02-03 13:36:51 -0500

    added styling and component for markdown editor box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class EditorArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div className="editor-wrapper">
+        <div class="toolbar">Editor</div>
+        <textarea id="editor">Placeholder text for editor</textarea>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EditorArea />
     </div>
   );
 }
